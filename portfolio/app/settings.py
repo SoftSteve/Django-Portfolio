cba726267d8f125cfba84a56ea36b937b848b817
SoftSteve <stevenhouk14@gmail.com>
2025-05-06 09:44:03 -0400

    Update

--- portfolio/app/settings.py
+++ portfolio/app/settings.py
@@ -27,9 +27,9 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['www.softsteve.com', 'softsteve.com', 'softsteve.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
